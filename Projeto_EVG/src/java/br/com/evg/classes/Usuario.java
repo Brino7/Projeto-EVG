cd2a2a1490a11328693c4cced669d6accba5741b
brino7 <brino7@hotmail.com>
2019-08-09 11:06:28 -0300

    Concerto da classe usuario para melhor atender o projeto, com base no projeto do professor joao.

--- Projeto_EVG/src/java/br/com/evg/classes/Usuario.java
+++ Projeto_EVG/src/java/br/com/evg/classes/Usuario.java
@@ -2,22 +2,30 @@ package br.com.evg.classes;
 
 import java.io.Serializable;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * @Author: Anderson Silva Brino 
  * @Data: 14/06/2019
  * @Hora: 18:43:54
  */
+@Entity
 public class Usuario extends Pessoa implements Serializable{
-    private Integer cod_Usuario;
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long cod_Usuario;
     private String loguin, senha;
     private List tipo_Usuario;
 
-    public Integer getCod_Usuario() {
+    public Long getCod_Usuario() {
         return cod_Usuario;
     }
 
-    public void setCod_Usuario(Integer cod_Usuario) {
+    public void setCod_Usuario(Long cod_Usuario) {
         this.cod_Usuario = cod_Usuario;
     }
 
@@ -45,5 +53,28 @@ public class Usuario extends Pessoa implements Serializable{
         this.tipo_Usuario = tipo_Usuario;
     }
     
-    
+   @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cod_Usuario != null ? cod_Usuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.cod_Usuario == null && other.cod_Usuario != null) || (this.cod_Usuario != null && !this.cod_Usuario.equals(other.cod_Usuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.com.provas.classes.Usuario[ id=" + cod_Usuario + " ]";
+    } 
 }
