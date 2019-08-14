d39e816792a39e583a28984eb10f4e1727546eea
brino7 <brino7@hotmail.com>
2019-08-14 14:22:49 -0300

    Correção da classe foto.

--- Projeto_EVG/src/java/br/com/evg/classes/Fotos.java
+++ Projeto_EVG/src/java/br/com/evg/classes/Fotos.java
@@ -1,13 +1,21 @@
 package br.com.evg.classes;
 
 import java.util.Calendar;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * @Author: Anderson Silva Brino 
  * @Data: 14/06/2019
  * @Hora: 17:23:32
  */
+@Entity
 public class Fotos {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer cod_Foto;
     private Object foto;
     private Membro cod_Membro;
@@ -72,15 +80,28 @@ public class Fotos {
         this.data = data;
     }
     
-    public void cadastrar_Foto(){
-        
+   @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cod_Foto != null ? cod_Foto.hashCode() : 0);
+        return hash;
     }
-    
-    public void remover_Foto(){
-        
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fotos)) {
+            return false;
+        }
+        Fotos other = (Fotos) object;
+        if ((this.cod_Foto == null && other.cod_Foto != null) || (this.cod_Foto != null && !this.cod_Foto.equals(other.cod_Foto))) {
+            return false;
+        }
+        return true;
     }
-    
-    public void buscar_Foto(){
-        
+
+    @Override
+    public String toString() {
+        return "br.com.provas.classes.Fotos[ id= " + cod_Foto + " ]";
     }
 }
