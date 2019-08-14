43af1efb1dd1f7c979efd60937401c40aeb8f9b4
brino7 <brino7@hotmail.com>
2019-08-14 14:57:58 -0300

    Correção da classe Projeto.

--- Projeto_EVG/src/java/br/com/evg/classes/Projeto.java
+++ Projeto_EVG/src/java/br/com/evg/classes/Projeto.java
@@ -1,11 +1,21 @@
 package br.com.evg.classes;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @Author: Anderson Silva Brino 
  * @Data: 14/06/2019
  * @Hora: 18:44:39
  */
-public class Projeto {
+@Entity
+public class Projeto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer cod_Projeto;
     private String nome;
 
@@ -25,19 +35,28 @@ public class Projeto {
         this.nome = nome;
     }
     
-    public void cadastrar_Projeto(){
-        
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cod_Projeto != null ? cod_Projeto.hashCode() : 0);
+        return hash;
     }
-    
-    public void remover_Projeto(){
-        
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Projeto)) {
+            return false;
+        }
+        Projeto other = (Projeto) object;
+        if ((this.cod_Projeto == null && other.cod_Projeto != null) || (this.cod_Projeto != null && !this.cod_Projeto.equals(other.cod_Projeto))) {
+            return false;
+        }
+        return true;
     }
-    
-    public void buscar_Projeto(){
-        
-    }
-    
-    public void editar_Projeto(){
-        
+
+    @Override
+    public String toString() {
+        return "br.com.provas.classes.Projeto[ id= " + cod_Projeto + " ]";
     }
 }
