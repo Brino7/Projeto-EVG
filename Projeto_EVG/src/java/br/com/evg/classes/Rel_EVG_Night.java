7cdfc1e7f6ca7ef9d141bbae9ed394dfaff12f4d
brino7 <brino7@hotmail.com>
2019-08-21 14:35:19 -0300

    Criação do relatorio do grupo evg night.

---
+++ Projeto_EVG/src/java/br/com/evg/classes/Rel_EVG_Night.java
@@ -0,0 +1,12 @@
+package br.com.evg.classes;
+
+import java.io.Serializable;
+
+/**
+ * @Author: Anderson Silva Brino 
+ * @Data: 21/08/2019
+ * @Hora: 14:24:50
+ */
+public class Rel_EVG_Night extends Relatorio implements Serializable{
+    
+}
